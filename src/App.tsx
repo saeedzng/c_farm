38b18b2be0822e0563642d0f1f8946d122f67453
Saeid <saeed.zng@gmail.com>
2024-10-29 17:51:12 +0330

    make wallet page dynamyc

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,12 @@ function App() {
   const [isdeployed , setIsdeployed] = useState <number>(0);
   const [referal_address, setReferal_address] = useState("EQDkzMK31Gn9nad9m1jnhEXXl8nKHJCf4006iyP6lSNyGs2C");
   const [walletContractAddress, setWalletContractAddress] = useState<string>("0QDAz5XMJoGW3TJE8a6QwreoTTGjPcPGvAOWm_yD1_k-SyUO");
-  const [  , setWalletData] = useState<null |{
-    // w_contract_address:string | undefined;
+  const [walletData , setWalletData] = useState<null |{
+    w_contract_address:string | undefined;
     w_contract_balance:number | null;
-    // w_owner_address: Address | undefined;
-    // w_referal_address:Address | undefined;
-    // w_master_address:Address | undefined;
+    w_owner_address: Address | undefined;
+    w_referal_address:Address | undefined;
+    w_master_address:Address | undefined;
   } >();
   useEffect(() => {
     const walletAddressFromUrl = window.Telegram.WebApp.initDataUnsafe.start_param;
@@ -51,21 +51,18 @@ function App() {
   useEffect(() =>{
 
       if (isdeployed == 1) {
-        const { wallet_contract_balance } = useWalletContract(Address.parse(walletContractAddress));
-        setWalletData({w_contract_balance : wallet_contract_balance})
         // setWalletData (null);
-        // const val = useWalletContract(Address.parse(walletContractAddress));
-        // setWalletData({
-        // wallet_contract_address : val.wallet_contract_address,
-        // wallet_contract_balance : val.wallet_contract_balance,
-        // wallet_owner_address : val.owner_address,
-        // wallet_referal_address : val.owner_address,
-        // wallet_master_address : val.master_address,
-        // });   
+        const val = useWalletContract(Address.parse("0QDAz5XMJoGW3TJE8a6QwreoTTGjPcPGvAOWm_yD1_k-SyUO"));
+        setWalletData({
+        w_contract_address : val.wallet_contract_address,
+        w_contract_balance : val.wallet_contract_balance,
+        w_owner_address : val.owner_address,
+        w_referal_address : val.owner_address,
+        w_master_address : val.master_address,
+        });   
         setcheck(1); 
       } 
   } ,[isdeployed] )
-  
 
   // const { ch_number, eggs_number, wallet_contract_balance, wallet_contract_address, send_buy_chicken_order, wallet_owner_address, wallet_referal_address, wallet_master_address, send_sell_chicken_order, send_recive_eggs_order } = useWalletContract(Address.parse(walletContractAddress));
 
@@ -125,7 +122,7 @@ function App() {
         <div>
           <h1>Wallet Contract</h1>
           <p>is deployed = {isdeployed}</p>
-          {/* <p> wallet address = {walletData?.wallet_contract_address && <div className='Hint'>{1} ton</div>} </p> */}
+          <p> wallet address = {walletData?.w_contract_address && <div className='Hint'>{1} ton</div>} </p>
           
           
           
