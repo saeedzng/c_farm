e371147dc516523af1984724d86989c0ea9e75be
Saeid <saeed.zng@gmail.com>
2024-11-06 19:29:40 +0330

    add warning to get eggs func

--- src/App.tsx
+++ src/App.tsx
@@ -107,9 +107,14 @@ function App() {
   };
 
   function warningloweggs () {
+
     WebApp.showConfirm(
-      "If this transaction brings you less than one egg or equivalent to 0.03 tons, the transaction will fail to avoid additional fees. Please refrain from confirming transactions that are predicted to fail. Keep in mind that each transaction in your network will have a fee of approximately 0.002 tons."
- 
+      "If this transaction brings you less than one egg or equivalent to 0.03 tons, the transaction will fail to avoid additional fees. Please refrain from confirming transactions that are predicted to fail. Keep in mind that each transaction in your network will have a fee of approximately 0.002 tons.",
+      (confirmed:boolean) => {
+        if (confirmed) {
+          WebApp.showAlert('test3')
+        }
+      }
     );
   };
   
