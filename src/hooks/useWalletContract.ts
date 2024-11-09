627ff734be11e3bcd46ae6c4553b44fcca1fbda2
Saeid <saeed.zng@gmail.com>
2024-11-09 15:20:49 +0330

    edit withdraw button

--- src/hooks/useWalletContract.ts
+++ src/hooks/useWalletContract.ts
@@ -56,14 +56,14 @@ export function useWalletContract(UserAddress: Address) {
     wallet_master_address: contractData?.master_address?.toString({ bounceable: false, testOnly: true }),
     ...contractData,
     send_buy_chicken_order: (chicken_to_buy: number) => {
-      return walletContract?.send_buy_chicken_order(sender, toNano(chicken_to_buy * 0.1), chicken_to_buy);
+      return walletContract?.send_buy_chicken_order(sender, toNano((chicken_to_buy * 0.1) + 0.01), chicken_to_buy);
     },
     send_buy_chicken_by_eggs: (chicken_to_buy: number) => {
       return walletContract?.send_buy_chicken_order(sender, toNano(0.01), chicken_to_buy);
     },
-    send_sell_chicken_order: (chicken_to_sell: number) => {
-      return walletContract?.send_sell_chicken_order(sender, toNano(0.01), chicken_to_sell);
-    },
+    // send_sell_chicken_order: (chicken_to_sell: number) => {
+    //   return walletContract?.send_sell_chicken_order(sender, toNano(0.01), chicken_to_sell);
+    // },
     send_recive_eggs_order: () => {
       return walletContract?.send_recive_eggs_order(sender, toNano(0.01));
     },
