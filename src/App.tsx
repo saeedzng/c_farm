7d2d85c2c3c3ae4dfe673c603820399def3eb5ce
Saeid <saeed.zng@gmail.com>
2024-11-12 18:47:53 +0330

    get owner address from tonbutton

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,7 @@ function App() {
     setIsdeployed(deployedValue);
   }, []);
 
+  const bbbbbb = useTonAddress()
   // useEffect(() => {
   //   if (connected) {
   //     setOwner_address(useTonAddress());
@@ -276,7 +277,7 @@ function App() {
             <button className="action-button" onClick={() => localStorage.clear()}>delete local storage</button><br />
             <input type="text" value={MwithdrawAmount} onChange={(e) => setMwithdrawAmount(e.target.value)}></input><br />
             <button className='action-button' onClick={() => {send_withdraw_order(Number(MwithdrawAmount))}}>withdraw</button><br />
-            <button className='action-button' onClick={() => {WebApp.showAlert(getOwnerTonAddress() + "---" + useTonAddress()) }}>show alert</button><br />
+            <button className='action-button' onClick={() => {WebApp.showAlert(getOwnerTonAddress() + "---" + bbbbbb) }}>show alert</button><br />
           </div>
         )}
         {page_n === 2 && (
